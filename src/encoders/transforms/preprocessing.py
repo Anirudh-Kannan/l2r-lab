83276088621fe2e8de0285ec76f7c1719f8c7982
Anirudh Kannan <anirudh@Anirudhs-MacBook-Pro.local>
2023-03-30 19:11:14 -0400

    Made canny preprocess step

--- src/encoders/transforms/preprocessing.py
+++ src/encoders/transforms/preprocessing.py
@@ -7,6 +7,7 @@ from src.constants import DEVICE
 
 def crop_resize_center(img):
     assert img.shape == (384, 512, 3)
+    img = cv2.Canny(x, 100, 200)
     p = cv2.resize(img, (144, 144))[68:110] / 255
     x = p.transpose(2, 0, 1)  # (H, W, C) --> (C, H, W)
     x = torch.as_tensor(x, device=DEVICE, dtype=torch.float)
