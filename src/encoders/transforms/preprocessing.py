ee3fd966f788d79f933d8020688625eabd9e4069
Anirudh Kannan <anirudhkannan25@gmail.com>
2023-03-31 19:15:22 +0000

    Added canny edge detector as a preprocessing step

--- src/encoders/transforms/preprocessing.py
+++ src/encoders/transforms/preprocessing.py
@@ -7,7 +7,8 @@ from src.constants import DEVICE
 
 def crop_resize_center(img):
     assert img.shape == (384, 512, 3)
-    img = cv2.Canny(x, 100, 200)
+    img = cv2.Canny(img, 100, 200)
+    img = np.repeat(img[:, :, np.newaxis], 3, axis=2)
     p = cv2.resize(img, (144, 144))[68:110] / 255
     x = p.transpose(2, 0, 1)  # (H, W, C) --> (C, H, W)
     x = torch.as_tensor(x, device=DEVICE, dtype=torch.float)
